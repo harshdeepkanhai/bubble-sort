81397e24469ccec1ea7a8060be63d2d644a3b5d1
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 03:32:51 -0600

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,17 @@
 def bubble_sort(arr)
-    n = arr.size
-    (0...n).each |i|
-      swapped = false
-      # as last ith elements get sorted in the end as bubbles
-      (0...n - i - 1).each |j| 
-        if arr[j] > arr[j + 1]
-          arr[j], arr[j + 1 ] = arr[j + 1], arr[j]
-          swapped = true
-        end
+  n = arr.size
+  (0...n).each do |i|
+    swapped = false
+    # as last ith elements get sorted in the end as bubbles
+    (0...n - i - 1).each do |j|
+      if arr[j] > arr[j + 1]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        swapped = true
       end
-      break if swapped == false
     end
-    arr
+    break if swapped == false
   end
-  
-  p bubble_sort([32, 4, 1312, 12, 45])
+  arr
+end
+
+p bubble_sort([32, 4, 1312, 12, 45])
