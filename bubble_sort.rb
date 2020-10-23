ce21a4c61727000ac5b94014570ab96e75f1360c
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 04:16:46 -0600

    Add bubble sort by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,17 @@ def bubble_sort(arr)
 end
 
 p bubble_sort([32, 4, 1312, 12, 45])
+
+def bubble_sort_by(arr)
+  n = arr.size
+  (0...n).each do |i|
+    swapped = false
+    (0...n - i - 1).each do |j|
+      arr[j], arr[j + 1], swapped = arr[j + 1], arr[j], true if yield(arr[j], arr[j + 1]).positive?
+    end
+    break if swapped == false
+  end
+  arr
+end
+
+p bubble_sort_by(%w[hi hello hey]) { |left, right| right.length - left.length }
