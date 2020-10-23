a122c7cf65cbbfe5dd42f7e428406ac691d18dcc
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 13:03:20 -0600

    fix offences and remove debug code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,6 @@ def bubble_sort(arr)
   n = arr.size
   (0...n).each do |i|
     swapped = false
-    # as last ith elements get sorted in the end as bubbles
     (0...n - i - 1).each do |j|
       if arr[j] > arr[j + 1]
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
@@ -13,8 +12,6 @@ def bubble_sort(arr)
   end
   arr
 end
-
-p bubble_sort([32, 4, 1312, 12, 45])
 
 def bubble_sort_by(arr)
   n = arr.size
@@ -27,5 +24,3 @@ def bubble_sort_by(arr)
   end
   arr
 end
-
-p bubble_sort_by(%w[hi hello hey]) { |left, right| right.length - left.length }
