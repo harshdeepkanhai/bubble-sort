d20f37a4b30a637cea302bc81a357e3afab3dd3a
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 03:08:20 -0600

    Add bubble sort algorithm

---
+++ bubble_sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(arr)
+    n = arr.size
+    for i in 0...n
+      swapped = false
+      for j in 0...n-i-1
+        if arr[j] > arr[j+1]
+          arr[j], arr[j+1] = arr[j+1], arr[j]
+          swapped = true
+        end
+      end
+      break if swapped == false
+    end
+    arr
+  end
+  
+  p bubble_sort([32,4,1312,12,45])
