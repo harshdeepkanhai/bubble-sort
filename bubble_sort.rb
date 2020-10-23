30de9cb28445692020f1102f6ae36e91e927056c
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 03:18:21 -0600

    Fix Rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,9 @@
 def bubble_sort(arr)
     n = arr.size
-    for i in 0...n
+    (0...n).each |i|
       swapped = false
       # as last ith elements get sorted in the end as bubbles
-      for j in 0...n-i-1 
+      (0...n-i-1).each |j| 
         if arr[j] > arr[j+1]
           arr[j], arr[j+1] = arr[j+1], arr[j]
           swapped = true
