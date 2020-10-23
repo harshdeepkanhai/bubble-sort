fe83a112b15e0202b7306bfddbef16a0e5cabc09
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-10-23 03:21:44 -0600

    Fix linter error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,7 +5,7 @@ def bubble_sort(arr)
       # as last ith elements get sorted in the end as bubbles
       (0...n - i - 1).each |j| 
         if arr[j] > arr[j + 1]
-          arr[j], arr[j +1 ] = arr[j + 1], arr[j]
+          arr[j], arr[j + 1 ] = arr[j + 1], arr[j]
           swapped = true
         end
       end
